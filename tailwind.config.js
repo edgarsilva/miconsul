459dbbaf8732097e3e53c7e65b5c20c3616a12da
edgarsilva <edgar.osc@gmail.com>
2024-05-04 12:29:07 -0600

    Better design for Blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
   },
   safelist: ["outline-2", "outline-4", "lg:text-4xl"],
   daisyui: {
-    themes: ["cmyk", "dracula", "night"],
+    themes: [
+      {
+        cmyk: {
+          ...require("daisyui/src/theming/themes")["cmyk"],
+          "base-200": "#f5f5f4",
+        },
+      },
+      "dracula",
+      "night",
+    ],
   },
   // plugins: [require("@tailwindcss/forms")],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
