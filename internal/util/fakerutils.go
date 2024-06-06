b1a7e318cc83f51094066d6561fb5734e07fb010
edgarsilva <edgar.osc@gmail.com>
2024-06-06 10:33:47 -0600

    Add more faker profile url utils.

--- internal/util/fakerutils.go
+++ internal/util/fakerutils.go
@@ -7,3 +7,15 @@ func PravatarURL(id string) string {
 func DicebearShapeAvatarURL(id string) string {
 	return "https://api.dicebear.com/8.x/shapes/svg?seed=" + id
 }
+
+func DicebearFunEmojiAvatarURL(id string) string {
+	return "https://api.dicebear.com/8.x/fun-emoji/svg?seed=" + id
+}
+
+func DicebearAvatarURL(id string) string {
+	return "https://api.dicebear.com/8.x/avataaars-neutral/svg?seed=" + id
+}
+
+func DicebearIconAvatarURL(id string) string {
+	return " https://api.dicebear.com/8.x/icons/svg?seed=" + id
+}
