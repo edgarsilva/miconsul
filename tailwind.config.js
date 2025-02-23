ee2cc60505e64864e9114013b62b210f5a035b62
edgarsilva <edgar.osc@gmail.com>
2025-02-22 18:25:14 -0600

    upd(tailwind-V4): Moving to tailwind 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./internal/**/*.{html,templ}"],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  safelist: ["outline-2", "outline-4", "lg:text-4xl"],
-  daisyui: {
-    themes: [
-      {
-        cmyk: {
-          ...require("daisyui/src/theming/themes")["cmyk"],
-          "base-200": "#f5f5f5",
-        }
-      },
-      "dracula",
-    ],
-  },
-  // plugins: [require("@tailwindcss/forms")],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+	content: ["./internal/**/*.{html,templ}"],
+	daisyui: {},
+	theme: {
+		extend: {},
+	},
+	// plugins: [require("@tailwindcss/forms")],
+	plugins: [],
 };
